35afb0d6b5065b0079275500f060653b2e99f04c
Eduardo Xerxenevsky <eduardoxer@gmail.com>
2024-05-17 19:50:32 -0300

    Delete z directory

--- z/src/main/java/br/senacsp/z/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package br.senacsp.z;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-    boolean existsByEmail(String email);
-    Profile findByEmail(String email);
-}
